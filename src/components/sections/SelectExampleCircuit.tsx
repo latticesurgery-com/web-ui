21190da1acd4f3ff7cf898d7ac24503dffc02547
Keelando W <47836064+Keelando@users.noreply.github.com>
2022-01-15 03:29:12 -0800

    Remove duplication in select circuit (#81)
    * seperate out submit compile request as its own function and file.
    
    * add arguments to submit compile request in UploadCircuit
    
    * call newly refactored submitcompilerequest and add correct function arguments.
    
    * lint

--- src/components/sections/SelectExampleCircuit.tsx
+++ src/components/sections/SelectExampleCircuit.tsx
@@ -2,17 +2,7 @@ import { AppStateProps } from "../../appState"
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react"
 import React from "react"
-import axios from "axios"
-
-import { CompilationResult } from "../../slices"
-import {
-    JsonParseError,
-    ApiHttpError,
-    CompilationResultSuccess,
-    NoServerResponse,
-    CompilerError,
-} from "../../apiResponses"
-import queryString from "query-string"
+import submitCompileRequest from "../submitCompileRequest"
 import Loader from "../Loader"
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -45,87 +35,7 @@ const SelectExampleCircuit = ({ appState, setAppState }: AppStateProps): JSX.Ele
     }
     readCircuitFile(exampleCircuit)
 
-    // To Do: forbid compile request submit with empty circuit
-    const submitCompileRequest = () => {
-        setExampleIsLoading(true)
-        const queryStringMap = queryString.parse(window.location.search)
-        const apiUrl = queryStringMap.localapi
-            ? `http://localhost:${queryStringMap.port || 9876}/compile`
-            : "https://api.latticesurgery.com/compile"
-
-        // Modify State on Compile Request Submit
-        setAppState({
-            ...appState,
-            compilationIsLoading: true,
-            apiResponse: null,
-        })
-
-        // Async JS HTTP request to API endpoint, apiUrl
-        axios
-            .post(apiUrl, {
-                circuit_source: "str",
-                circuit: circuitStr,
-                apply_litinski_transform: doLitinskiTransform,
-            })
-            .then((response) => {
-                if (response.data.errorMessage) {
-                    const errortype = response.data.errorType
-                    const msg = response.data.errorMessage
-                    setAppState({
-                        apiResponse: new CompilerError(errortype, msg),
-                        compilationIsLoading: false,
-                    })
-                    setExampleIsLoading(false)
-                } else {
-                    try {
-                        const responseJson = JSON.parse(response.data) as CompilationResult
-                        setAppState({
-                            apiResponse: new CompilationResultSuccess(
-                                responseJson.slices,
-                                responseJson.compilation_text
-                            ),
-                            compilationIsLoading: false,
-                        })
-                        setExampleIsLoading(false)
-                    } catch (error) {
-                        setAppState({
-                            apiResponse: new JsonParseError((error as Error).toString()),
-                            compilationIsLoading: false,
-                        })
-                        setExampleIsLoading(false)
-                    }
-                }
-
-                // Catch Axios Errors: API errors -> timeout, error compiling, or no response
-            })
-            .catch((error) => {
-                // needs more testing. First two requests after a long lambda cooldown period result in CORS / Network Errors
-                if (error.response) {
-                    console.error("AXIOS error:")
-                    console.log(error.response.data)
-                    console.log(error.response.headers)
-                    console.log(error.response.status)
-                    const error_code = error.response.status
-                    const error_data = error.response.data
-                    const error_headers = error.response.headers
-                    // var api_error = new ApiHttpError(error_code,error_data,error_headers)
-                    setAppState({
-                        // apiResponse: new ApiHttpError((error as Error).toString()),
-                        apiResponse: new ApiHttpError(error_code, error_data, error_headers),
-                        compilationIsLoading: false,
-                    })
-                    setExampleIsLoading(false)
-                } else {
-                    setAppState({
-                        // apiResponse: new ApiHttpError((error as Error).toString()),
-                        apiResponse: new NoServerResponse("Server did not respond"),
-                        compilationIsLoading: false,
-                    })
-                    setExampleIsLoading(false)
-                }
-            })
-    }
-
+    // TODO: forbid compile request submit with empty circuit
     return (
         <div>
             Or choose from the following example circuits:
@@ -154,7 +64,14 @@ const SelectExampleCircuit = ({ appState, setAppState }: AppStateProps): JSX.Ele
                             `}
                             className="btn btn-primary"
                             disabled={appState.compilationIsLoading}
-                            onClick={() => submitCompileRequest()}
+                            onClick={() =>
+                                submitCompileRequest(
+                                    { appState, setAppState },
+                                    circuitStr as string,
+                                    setExampleIsLoading,
+                                    doLitinskiTransform
+                                )
+                            }
                         >
                             {appState.compilationIsLoading && exampleIsLoading ? (
                                 <Loader size={20} color="white" />
--- src/components/sections/UploadCircuit.tsx
+++ src/components/sections/UploadCircuit.tsx
@@ -4,18 +4,9 @@ import { css } from "@emotion/react"
 
 import { AppStateProps } from "../../appState"
 import Loader from "../../components/Loader"
-import { CompilationResult } from "../../slices"
 
 import SelectExampleCircuit from "./SelectExampleCircuit"
-import axios from "axios"
-import {
-    JsonParseError,
-    ApiHttpError,
-    CompilationResultSuccess,
-    NoServerResponse,
-    CompilerError,
-} from "../../apiResponses"
-import queryString from "query-string"
+import submitCompileRequest from "../submitCompileRequest"
 
 const UploadACircuit = ({ appState, setAppState }: AppStateProps) => {
     const [uploadIsLoading, setUploadIsLoading] = React.useState(false)
@@ -36,86 +27,6 @@ const UploadACircuit = ({ appState, setAppState }: AppStateProps) => {
     }
 
     // To Do: forbid compile request submit with empty circuit
-    const submitCompileRequest = () => {
-        setUploadIsLoading(true)
-        const queryStringMap = queryString.parse(window.location.search)
-
-        const apiUrl = queryStringMap.localapi
-            ? `http://localhost:${queryStringMap.port || 9876}/compile`
-            : "https://api.latticesurgery.com/compile"
-
-        // Modify State on Compile Request Submit
-        setAppState({
-            ...appState,
-            compilationIsLoading: true,
-            apiResponse: null,
-        })
-
-        // Async JS HTTP request to API endpoint, apiUrl
-        axios
-            .post(apiUrl, {
-                circuit_source: "str",
-                circuit: circuitStr,
-                apply_litinski_transform: doLitinskiTransform,
-            })
-            .then((response) => {
-                if (response.data.errorMessage) {
-                    const errortype = response.data.errorType
-                    const msg = response.data.errorMessage
-                    setAppState({
-                        apiResponse: new CompilerError(errortype, msg),
-                        compilationIsLoading: false,
-                    })
-                    setUploadIsLoading(false)
-                } else {
-                    try {
-                        const responseJson = JSON.parse(response.data) as CompilationResult
-                        setAppState({
-                            apiResponse: new CompilationResultSuccess(
-                                responseJson.slices,
-                                responseJson.compilation_text
-                            ),
-                            compilationIsLoading: false,
-                        })
-                        setUploadIsLoading(false)
-                    } catch (error) {
-                        setAppState({
-                            apiResponse: new JsonParseError((error as Error).toString()),
-                            compilationIsLoading: false,
-                        })
-                        setUploadIsLoading(false)
-                    }
-                }
-
-                // Catch Axios Errors: API errors -> timeout, error compiling, or no response
-            })
-            .catch((error) => {
-                // needs more testing. First two requests after a long lambda cooldown period result in CORS / Network Errors
-                if (error.response) {
-                    console.error("AXIOS error:")
-                    console.log(error.response.data)
-                    console.log(error.response.headers)
-                    console.log(error.response.status)
-                    const error_code = error.response.status
-                    const error_data = error.response.data
-                    const error_headers = error.response.headers
-                    // var api_error = new ApiHttpError(error_code,error_data,error_headers)
-                    setAppState({
-                        // apiResponse: new ApiHttpError((error as Error).toString()),
-                        apiResponse: new ApiHttpError(error_code, error_data, error_headers),
-                        compilationIsLoading: false,
-                    })
-                    setUploadIsLoading(false)
-                } else {
-                    setAppState({
-                        // apiResponse: new ApiHttpError((error as Error).toString()),
-                        apiResponse: new NoServerResponse("Server did not respond"),
-                        compilationIsLoading: false,
-                    })
-                    setUploadIsLoading(false)
-                }
-            })
-    }
 
     return (
         <div className="mb-3">
@@ -136,7 +47,14 @@ const UploadACircuit = ({ appState, setAppState }: AppStateProps) => {
                             `}
                             className="btn btn-primary"
                             disabled={appState.compilationIsLoading}
-                            onClick={() => submitCompileRequest()}
+                            onClick={() =>
+                                submitCompileRequest(
+                                    { appState, setAppState },
+                                    circuitStr as string,
+                                    setUploadIsLoading,
+                                    doLitinskiTransform
+                                )
+                            }
                         >
                             {appState.compilationIsLoading && uploadIsLoading ? (
                                 <Loader size={20} color="white" />
