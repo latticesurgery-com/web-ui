b7a6bba1dd95d0decb058fb77dad3bba60017454
Keelan <keelan_w@outlook.com>
2022-01-21 14:38:32 -0800

    fix lint errors

--- src/pages/Base.tsx
+++ src/pages/Base.tsx
@@ -1,9 +1,7 @@
 import { Container } from "@chakra-ui/react"
 import Navbar from "../components/UI/Navbar"
 
-interface MyProps { }
-
-const Base = (props: React.PropsWithChildren<MyProps>) => {
+const Base = (props: React.PropsWithChildren<Record<never, never>>) => {
     return (
         <Container maxW="container.xl">
             <Navbar />
