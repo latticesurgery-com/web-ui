7bc4805e1b28e1339adc81c056c9d7d906aa16e1
Keelan <keelan_w@outlook.com>
2022-03-01 04:07:40 -0800

    scale eigenstate fontsize with slider

--- src/components/Zoombar.tsx
+++ src/components/Zoombar.tsx
@@ -62,6 +62,7 @@ const ZoomBar = ({
                 onChange={(val) => {
                     setSliderValue(val)
                     setCellDimension(val)
+                    setCellFontSize(val/7.5)
                 }}
             >
                 <SliderTrack w="15px" rounded="xl">
