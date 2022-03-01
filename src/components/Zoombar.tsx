f4b6e6eb0b9d3a53dad9e6b11243d610db166762
Keelan <keelan_w@outlook.com>
2022-03-01 14:43:27 -0800

    add fade in/fade out to zoom percentage. Calculate zoom percentage base on original render size

--- src/components/Zoombar.tsx
+++ src/components/Zoombar.tsx
@@ -2,12 +2,14 @@ import {
     Button,
     SliderTrack,
     Slider,
+    SliderMark,
     SliderFilledTrack,
     SliderThumb,
     VStack,
 } from "@chakra-ui/react"
 import { useState } from "react"
 import { GrAddCircle, GrSubtractCircle } from "react-icons/gr"
+import "./Zoombar.css"
 
 type ZoomBarProps = {
     cellDimension: number
@@ -23,20 +25,26 @@ const ZoomBar = ({
     setCellFontSize,
 }: ZoomBarProps) => {
     const [sliderMax, sliderStep, defaultSliderValue] = [200, 10, 150]
-    const [sliderValue, setSliderValue] = useState(150)
+    const [sliderValue, setSliderValue] = useState(150) // 150 corresponds to 100% zoom.
     console.log("Slider Val", sliderValue)
+    const [sliderMoving, setSliderMoving] = useState(false)
+    console.log("slider moving?", sliderMoving)
     const zoomChange = (direction: number) => {
         if (direction > 0) {
             if (sliderValue !== sliderMax) {
                 setSliderValue(sliderValue + 10)
                 setCellDimension(cellDimension + 10)
                 setCellFontSize(cellFontSize + 1.5)
+                setSliderMoving(true)
+                setTimeout(() => setSliderMoving(false), 1000)
             }
         } else if (direction < 0) {
             if (sliderValue !== 0) {
                 setSliderValue(sliderValue - 10)
                 setCellDimension(cellDimension - 10)
                 setCellFontSize(cellFontSize - 1.5)
+                setSliderMoving(true)
+                setTimeout(() => setSliderMoving(false), 1000)
             }
         }
     }
@@ -61,16 +69,32 @@ const ZoomBar = ({
                 step={sliderStep}
                 onChange={(val) => {
                     setSliderValue(val)
+                    setSliderMoving(true)
                 }}
                 onChangeEnd={(val) => {
                     setCellDimension(val)
                     setCellFontSize(val / 7.5)
+                    setTimeout(() => {
+                        setSliderMoving(false)
+                    }, 300)
                 }}
             >
-                <SliderTrack w="15px" rounded="xl">
+                <SliderMark
+                    value={sliderValue}
+                    textAlign="center"
+                    bg="blue.500"
+                    color="white"
+                    mt="-8"
+                    ml="4"
+                    w="12"
+                    className={sliderMoving ? "fade-in" : "fade-out"}
+                >
+                    {Math.round(sliderValue * 0.6667)}%
+                </SliderMark>
+                <SliderTrack w="8px" rounded="xl">
                     <SliderFilledTrack />
                 </SliderTrack>
-                <SliderThumb boxSize="6" />
+                <SliderThumb boxSize="5" onClick={(e) => setSliderMoving(true)} />
             </Slider>
         </VStack>
     )
