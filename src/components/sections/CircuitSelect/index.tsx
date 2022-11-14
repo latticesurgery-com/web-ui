32ebdcef5aa6907e270d34f6156abeee8e78b7c3
gwwatkin@sfu.ca <gwwatkin@sfu.ca>
2022-11-13 19:09:13 -0800

    Revert "Fix linting issue, disable lint req for build step"
    This reverts commit 88037afb8fdb64fa5cb8ac0c9d34102861722b71.

--- src/components/sections/CircuitSelect/index.tsx
+++ src/components/sections/CircuitSelect/index.tsx
@@ -35,141 +35,102 @@ import {
 } from "../../../lib/compilationOptions"
 import { Slices } from "../../../lib/slices"
 
+
 interface CompilationOptionsSelectorProps {
     compilationOptions: CompilationOptions
     setCompilationOptions: React.Dispatch<CompilationOptions>
 }
 
-const CompilationOptionsSelector = ({
-    compilationOptions,
-    setCompilationOptions,
-}: CompilationOptionsSelectorProps) => {
-    const updateFastSlicerOptions = (fastSlicerOptions: FastSlicerOptions) =>
-        setCompilationOptions({ ...compilationOptions, fastSlicerOptions })
-    const updateLitinskiCompilationOptions = (
-        litinskiCompilationOptions: LitinskiCompilationOptions
-    ) => setCompilationOptions({ ...compilationOptions, litinskiCompilationOptions })
+const CompilationOptionsSelector = ({compilationOptions, setCompilationOptions} : CompilationOptionsSelectorProps) => {
 
-    return (
-        <VStack>
-            <Select>
-                <option
-                    onClick={() =>
-                        setCompilationOptions({ ...compilationOptions, kind: "FastSlicerOptions" })
-                    }
-                >
-                    Fast Slicer with direct compilation via ZX
-                </option>
-                <option
-                    onClick={() =>
-                        setCompilationOptions({
-                            ...compilationOptions,
-                            kind: "LitinskiCompilationOptions",
-                        })
-                    }
-                >
-                    Litinski style compilation via Pauli Rotations
-                </option>
-            </Select>
-            {compilationOptions.kind === "FastSlicerOptions" ? (
-                <>
-                    <Text>
-                        <b>Note:</b> If submitting .qasm, the fast slicer only supports{" "}
-                        <Link
-                            color="teal.500"
-                            href="https://github.com/latticesurgery-com/liblsqecc#qasm-support-highly-experimental/"
-                            target="_blank"
-                            isExternal
-                        >
-                            a very small subset
-                        </Link>
-                        .
-                    </Text>
-                    <HStack>
-                        <Text>Layout:</Text>
-                        <Select
-                            defaultValue={
-                                LayoutType[compilationOptions.fastSlicerOptions.layoutType]
-                            }
-                        >
-                            {Object.keys(LayoutType)
-                                .filter((v) => isNaN(Number(v)))
-                                .map((item) => (
-                                    <option
-                                        key={item}
-                                        onClick={() =>
-                                            updateFastSlicerOptions({
-                                                ...compilationOptions.fastSlicerOptions,
-                                                layoutType:
-                                                    LayoutType[item as keyof typeof LayoutType],
-                                            })
-                                        }
-                                    >
-                                        {item}
-                                    </option>
-                                ))}
-                        </Select>
-                        <Text>Apply corrective terms:</Text>
-                        <Select
-                            defaultValue={
-                                CorrectiveTermBehaviour[
-                                    compilationOptions.fastSlicerOptions.correctiveTermBehaviour
-                                ]
-                            }
-                        >
-                            {Object.keys(CorrectiveTermBehaviour)
-                                .filter((v) => isNaN(Number(v)))
-                                .map((item) => (
-                                    <option
-                                        key={item}
-                                        onClick={() =>
-                                            updateFastSlicerOptions({
-                                                ...compilationOptions.fastSlicerOptions,
-                                                correctiveTermBehaviour:
-                                                    CorrectiveTermBehaviour[
-                                                        item as keyof typeof CorrectiveTermBehaviour
-                                                    ],
-                                            })
-                                        }
-                                    >
-                                        {item}
-                                    </option>
-                                ))}
-                        </Select>
-                    </HStack>
-                </>
-            ) : (
-                <Checkbox
-                    isChecked={
-                        compilationOptions.litinskiCompilationOptions.doStabilizerCommutingTransform
-                    }
-                    onChange={(e) =>
-                        updateLitinskiCompilationOptions({
+    const updateFastSlicerOptions = (fastSlicerOptions : FastSlicerOptions) =>
+        setCompilationOptions({...compilationOptions, fastSlicerOptions})
+    const updateLitinskiCompilationOptions= (litinskiCompilationOptions : LitinskiCompilationOptions) =>
+        setCompilationOptions({...compilationOptions, litinskiCompilationOptions})
+
+    return <VStack>
+        <Select>
+            <option
+                onClick={() => setCompilationOptions({ ...compilationOptions, kind: "FastSlicerOptions" })}>
+                Fast Slicer with direct compilation via ZX
+            </option>
+            <option
+                onClick={() => setCompilationOptions({ ...compilationOptions, kind: "LitinskiCompilationOptions" })}>
+                Litinski style compilation via Pauli Rotations
+            </option>
+        </Select>
+        {(compilationOptions.kind === "FastSlicerOptions")
+            ?
+            <>
+                <Text>
+                    <b>Note:</b> If submitting .qasm, the fast slicer only supports
+                    {' '}
+                    <Link
+                        color="teal.500"
+                        href="https://github.com/latticesurgery-com/liblsqecc#qasm-support-highly-experimental/"
+                        target="_blank"
+                        isExternal
+                    >
+                        a very small subset
+                    </Link>.
+                </Text>
+                <HStack>
+                    <Text>Layout:</Text>
+                    <Select defaultValue={LayoutType[compilationOptions.fastSlicerOptions.layoutType]}>
+                        {Object.keys(LayoutType).filter((v) => isNaN(Number(v))).map((item) =>
+                            <option
+                                key={item}
+                                onClick={() => updateFastSlicerOptions({
+                                    ...compilationOptions.fastSlicerOptions, layoutType : LayoutType[item as keyof typeof LayoutType]
+                                })}>
+                                {item}
+                            </option>
+                        )
+                        }
+                    </Select>
+                    <Text>Apply corrective terms:</Text>
+                    <Select defaultValue={CorrectiveTermBehaviour[compilationOptions.fastSlicerOptions.correctiveTermBehaviour]}>
+                        {Object.keys(CorrectiveTermBehaviour).filter((v) => isNaN(Number(v))).map((item) =>
+                            <option
+                                key={item}
+                                onClick={() => updateFastSlicerOptions({
+                                    ...compilationOptions.fastSlicerOptions, correctiveTermBehaviour : CorrectiveTermBehaviour[item as keyof typeof CorrectiveTermBehaviour]
+                                })}>
+                                {item}
+                            </option>
+                        )
+                        }
+                    </Select>
+                </HStack>
+            </>
+            :
+            <Checkbox
+                isChecked={compilationOptions.litinskiCompilationOptions.doStabilizerCommutingTransform}
+                onChange={(e) => updateLitinskiCompilationOptions({
                             ...compilationOptions.litinskiCompilationOptions,
-                            doStabilizerCommutingTransform: e.target.checked,
-                        })
-                    }
-                >
-                    <Text as={"span"}>Pi/4 removing transform</Text>
-                </Checkbox>
-            )}
-        </VStack>
-    )
+                            doStabilizerCommutingTransform: e.target.checked
+                        })}
+            >
+                <Text as={"span"}>Pi/4 removing transform</Text>
+            </Checkbox>
+        }
+    </VStack>
 }
 
 interface EmscriptenSlicerResult {
-    err: string
+    err : string
     exit_code: number
-    output: string
+    output : string
 }
 
+
 const runCompilation = async (
-    setAppState: React.Dispatch<AppState>,
-    fileContents: string,
-    compilationOptions: CompilationOptions,
+    setAppState:React.Dispatch<AppState>,
+    fileContents : string,
+    compilationOptions:CompilationOptions,
     extension: string,
-    repeats: number
-) => {
+    repeats: number) => {
+
     // Modify State on Compile Request Submit
     setAppState({
         compilationIsLoading: true,
@@ -177,14 +138,9 @@ const runCompilation = async (
     })
 
     if (compilationOptions.kind == "LitinskiCompilationOptions")
-        submitCompileRequest(
-            setAppState,
-            fileContents,
-            compilationOptions.litinskiCompilationOptions,
-            repeats
-        )
+        submitCompileRequest(setAppState, fileContents, compilationOptions.litinskiCompilationOptions, repeats)
     else {
-        const { fastSlicerOptions } = compilationOptions
+        const {fastSlicerOptions} = compilationOptions;
 
         // Modify State on Compile Request Submit
         setAppState({
@@ -198,21 +154,15 @@ const runCompilation = async (
 
         const commandLine = [
             fastSlicerOptions.layoutType === LayoutType.Compact ? "--compactlayout" : "",
-            extension === "qasm" ? "-q" : "",
+            extension ==="qasm" ? "-q" : "",
             "--cnotcorrections",
-            fastSlicerOptions.correctiveTermBehaviour === CorrectiveTermBehaviour.Allways
-                ? "always"
-                : "never",
+            fastSlicerOptions.correctiveTermBehaviour === CorrectiveTermBehaviour.Allways ? "always" : "never"
         ].join(" ")
-        if (isDevMode())
-            console.log(`Compiling in browser with args: ${commandLine}\ninput:\n${fileContents}`)
-        const emscriptenRawResult = loadedModule.run_slicer_program_from_strings(
-            commandLine,
-            fileContents
-        )
-        if (isDevMode()) console.log(emscriptenRawResult)
+        if(isDevMode()) console.log(`Compiling in browser with args: ${commandLine}\ninput:\n${fileContents}`)
+        const emscriptenRawResult = loadedModule.run_slicer_program_from_strings(commandLine, fileContents)
+        if(isDevMode()) console.log(emscriptenRawResult);
 
-        const emscriptenResult: EmscriptenSlicerResult = JSON.parse(emscriptenRawResult)
+        const emscriptenResult : EmscriptenSlicerResult = JSON.parse(emscriptenRawResult);
 
         try {
             if (emscriptenResult.exit_code === 0) {
@@ -221,16 +171,16 @@ const runCompilation = async (
                     apiResponse: new CompilationResultSuccess(
                         JSON.parse(emscriptenResult.output) as Slices,
                         `Emscripten Success`
-                    ),
+                    )
                 })
             } else {
                 setAppState({
                     compilationIsLoading: false,
                     apiResponse: new ResponseError(
                         `Compiler returned non zero exit code ${emscriptenResult.exit_code}`,
-                        `Compiler Error: ${emscriptenResult.err}\n` +
-                            (isDevMode() ? `Compiler Output: ${emscriptenResult.output}` : "")
-                    ),
+                        `Compiler Error: ${emscriptenResult.err}\n`
+                            + (isDevMode()?`Compiler Output: ${emscriptenResult.output}`:"")
+                    )
                 })
             }
         } catch (err) {
@@ -239,9 +189,11 @@ const runCompilation = async (
                 apiResponse: new ResponseError(
                     `JS Exception`,
                     `${err}\nCompiler Error: ${emscriptenResult.err}\nCompiler Output: ${emscriptenResult.output}`
-                ),
+                )
             })
         }
+
+
     }
 }
 
@@ -253,8 +205,7 @@ type CircuitSelectProps = {
 }
 
 const CircuitSelect = ({ appState, setAppState, repeats, setRepeats }: CircuitSelectProps) => {
-    const [compilationOptions, setCompilationOptions] =
-        useState<CompilationOptions>(defaultCompilationOptions)
+    const [compilationOptions, setCompilationOptions] = useState<CompilationOptions>(defaultCompilationOptions)
     const readFile = (file: File) => {
         return new Promise((resolve, reject) => {
             const reader = new FileReader()
@@ -284,13 +235,7 @@ const CircuitSelect = ({ appState, setAppState, repeats, setRepeats }: CircuitSe
                 })
             }
         } else {
-            await runCompilation(
-                setAppState,
-                data as string,
-                compilationOptions,
-                extension,
-                repeats
-            )
+            await runCompilation(setAppState, data as string, compilationOptions, extension, repeats)
         }
     }
 
@@ -329,10 +274,7 @@ const CircuitSelect = ({ appState, setAppState, repeats, setRepeats }: CircuitSe
                 onFileAccepted={onFileAccepted}
                 isLoading={appState.compilationIsLoading}
             />
-            <CompilationOptionsSelector
-                compilationOptions={compilationOptions}
-                setCompilationOptions={setCompilationOptions}
-            />
+            <CompilationOptionsSelector compilationOptions={compilationOptions} setCompilationOptions={setCompilationOptions} />
             {isDevMode() && (
                 <Box p="3" rounded="lg" borderWidth="3px" borderColor="#98ff98">
                     <Flex gap="2">
